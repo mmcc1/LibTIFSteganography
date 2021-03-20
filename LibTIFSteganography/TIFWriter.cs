fd468c7ea5bd302693fc16603560424521eb065f
Marx Bitware <info@marxbitware.co.uk>
2021-03-20 16:39:00 +0700

    Add project files.

---
+++ LibTIFSteganography/TIFWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+
+namespace LibTIFSteganography
+{
+    /*
+     * TIF
+     * 
+     * Encodes/Decodes data into TIF images using the Alpha channel.  
+     */
+
+    public class TIFWriter
+    {
+        public Bitmap Encode(Bitmap sourceTIFImage, byte[] data)
+        {
+            if (data.Length > sourceTIFImage.Width * sourceTIFImage.Height)
+                throw new Exception("Image to small.");
+
+            Bitmap encodedTIFImage = new Bitmap(sourceTIFImage);
+            int index = 0;
+
+            for(int i = 0; i < encodedTIFImage.Height; i++)
+            {
+                for(int j = 0; j < encodedTIFImage.Width; j++)
+                {
+                    if (index < data.Length)
+                    {
+                        Color pixel = encodedTIFImage.GetPixel(j, i);
+                        Color p = Color.FromArgb(data[index++], pixel.R, pixel.G, pixel.B);
+                        encodedTIFImage.SetPixel(j, i, p);
+                    }
+                    else
+                        break;
+                }
+            }
+
+            return encodedTIFImage;
+        }
+
+        public byte[] Decode(Bitmap encodedTIFImage, int size)
+        {
+            if (size > encodedTIFImage.Width * encodedTIFImage.Height)
+                throw new Exception("Image to small.");
+
+            byte[] result = new byte[size];
+            int index = 0;
+
+            for (int i = 0; i < encodedTIFImage.Height; i++)
+            {
+                for (int j = 0; j < encodedTIFImage.Width; j++)
+                {
+                    if (index < size)
+                    {
+                        Color pixel = encodedTIFImage.GetPixel(j, i);
+                        result[index++] = pixel.A;
+                    }
+                    else
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TIFTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using LibTIFSteganography;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace TIFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opening image...");
+            Image sourceImage = Bitmap.FromFile("images/scenic_beach.tif");
+            byte[] randomMessage = GenerateRandomBytes(sourceImage.Width * sourceImage.Height);
+            Bitmap bm = new Bitmap(sourceImage);
+
+            TIFWriter tif = new TIFWriter();
+            Bitmap bm2 = tif.Encode(bm, randomMessage);
+            bm2.Save("test.tif", ImageFormat.Tiff);
+
+            byte[] b3 = tif.Decode(bm2, randomMessage.Length);
+
+            if (b3.SequenceEqual(randomMessage));
+                Console.WriteLine("Encoded information recovered from image");
+
+            Console.WriteLine("File Written.  Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        public static byte[] GenerateRandomBytes(int size)
+        {
+            byte[] rngOutput = new byte[size];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(rngOutput);
+
+            return rngOutput;
+        }
+    }
+}
